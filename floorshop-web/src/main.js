11b8769e9b41ca4647c7e1e5a070e09b48d726d4
林龙辉 <540886643@qq.com>
2017-06-27 10:07:00 +0800

    * add babel-preset-react

--- floorshop-web/src/main.js
+++ floorshop-web/src/main.js
@@ -1,2 +1,6 @@
 
-console.log('Configuration Done!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
