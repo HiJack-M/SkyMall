be81b9098508ea2acaf75a3fba0775f8f14dacfe
林龙辉 <540886643@qq.com>
2017-06-28 16:32:16 +0800

    * bottom tabbar

--- floorshop-mobile/src/App.js
+++ floorshop-mobile/src/App.js
@@ -1,6 +1,10 @@
 
 import React from 'react'
+import { Route } from 'react-router-dom'
+// pages
 import Index from '@/pages/index'
+import Order from '@/pages/order'
+import Me from '@/pages/me'
 import Tabbar from '@/components/tabbar'
 
 import 'normalize.css/normalize.css'
@@ -9,7 +13,9 @@ export default class App extends React.Component {
   render () {
     return (
       <div className='App-Container'>
-        <Index />
+        <Route exact path='/' component={Index} />
+        <Route path='/order' component={Order} />
+        <Route path='/me' component={Me} />
         <Tabbar />
       </div>
     )
