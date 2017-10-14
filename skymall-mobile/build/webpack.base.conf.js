46f7c4e7ef13de2a2afcb58bc5101179b80c1ca7
林龙辉 <540886643@qq.com>
2017-10-14 08:27:38 +0800

    * update webpack setting;

--- skymall-mobile/build/webpack.base.conf.js
+++ skymall-mobile/build/webpack.base.conf.js
@@ -22,6 +22,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
+      'comps': resolve('src/components'),
       '@': resolve('src')
     }
   },
